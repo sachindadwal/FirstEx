ef7ec83824aeb48f4d8d18f3cc6f1c9454a60d7e
sachindadwal <90467353+sachindadwal@users.noreply.github.com>
2021-09-16 09:55:30 +0000

    new

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Sachin");
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+var userName = readlineSync.question('What is your name? ');
+console.log('Hello ' + userName + '!');
